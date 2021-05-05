a77f1e01878269f21b0ae1c05b154897a88e78e2
Yash728 <61221867+Yash728@users.noreply.github.com>
2021-05-06 02:13:51 +0530

    added to do

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
 var encoder = bodyParser.urlencoded();
+const mongoose = require('mongoose')
+//model for submission page
+//login form connect to db
+//db connection
+//images from backend
+//a html page where all the data of submission can be seen
+//post route for submit
 app.use('/assets', express.static('assets'));
 
 app.get('/', function (req, res) {
