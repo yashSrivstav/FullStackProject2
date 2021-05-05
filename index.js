e5ba2e76a4d8c2dab16677781f455d1cc4958961
Yash728 <61221867+Yash728@users.noreply.github.com>
2021-05-06 00:37:45 +0530

    Project 2 started everything initialized

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const app=express()
+var bodyParser=require('body-parser')
+var encoder=bodyParser.urlencoded();
+app.set('view engine','ejs')
+app.use('/assets',express.static('assets'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html')
+})
+app.get('/team',(req,res)=>{
+    res.sendFile(__dirname+'/HTML/team.html')
+})
+app.listen(4500);
